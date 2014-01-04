1b7df55f0820d8cc9d2616d0ec8a48fdccd81e88
craig.brandstetter <cbrandst@gmail.com>
2014-01-04 16:14:17 -0600

    End of video 7.  Shots are being fired, but are going off into infinity.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,31 @@ public class Boundary
 	public float xMin, xMax, zMin, zMax;	
 }
 
-
 public class PlayerController : MonoBehaviour 
 {	
-	
 	public float speed;
-	public Boundary boundary;
 	public float tilt;
+	public Boundary boundary;
 	
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	
+	private float nextFire;
+	
+	void Update ()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+			//GameObject clone = 
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation); // as GameObject;
+		}
+		
+	}
 
 	void FixedUpdate ()
 	{
-
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 		
@@ -34,5 +47,4 @@ public class PlayerController : MonoBehaviour
 		
 		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
 	}
-	
 }
